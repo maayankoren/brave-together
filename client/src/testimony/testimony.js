0f20e3737897175d5245fd843c1ec39ffb10892c
ABC <dvivonbirati@gmail.com>
2021-09-24 15:56:51 +0300

    fixing handelSelectionChange

--- client/src/testimony/testimony.js
+++ client/src/testimony/testimony.js
@@ -101,7 +101,8 @@ class Testimony extends React.Component {
 
     handelSelectionChange = () => {
         //chack text "selection = window.getSelection().toString();"
-        const freeSelection = window.getSelection().toString()
+        const freeSelection = window.getSelection().toString();
+        if(freeSelection)
         this.setState({ freeSelection })
     }
 
