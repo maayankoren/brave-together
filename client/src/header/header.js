7784e341ca56c7bc97b4222b99f66dad2c1647c5
Ariye Caspi <caspi_j@hotmail.com>
2021-10-23 22:24:11 +0300

    .

--- client/src/header/header.js
+++ client/src/header/header.js
@@ -1,123 +1,78 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { NavLink } from "react-router-dom";
 import "./header.scss";
 
-class Header extends React.Component {
+const Header = () => {
 
-  constructor(props) {
-    super(props);
+  const [isSignedIn, setSignIn] = useState(false);
 
-    this.state = {
-      isSignedIn: false
-    }
-  }
-
-  componentDidMount() {
+  useEffect(() => {
     let token = localStorage.getItem('token');
 
     if (token) {
-      this.setState({ isSignedIn: true });
+      setSignIn(true);
+    } else {
+      setSignIn(false);
     }
+  }, [])
 
-    else {
-      this.setState({ isSignedIn: false });
-    }
+  let data = {
+    "header": {
+      "color": "blue",
+      "logo": "https://brave-together.com/wp-content/uploads/2020/04/%D7%A2%D7%95%D7%AA%D7%A7-%D7%A9%D7%9C-Copy-of-%D7%9C%D7%95%D7%92%D7%95-%D7%97%D7%93%D7%A9-%D7%A8%D7%A7%D7%A2-%D7%A9%D7%A7%D7%95%D7%A3.png",
+      "links": [
+        {
+          "name": "",
+          "diaplayName": "התחבר",
+          "rout": "/login",
+          "color": "white"
+        },
+        {
+          "name": "",
+          "diaplayName": "התנתק",
+          "rout": "/",
+          "color": "white"
+        },
+        {
+          "name": "",
+          "diaplayName": "בית",
+          "rout": "/",
+          "color": "white"
+        },
+        {
+          "name": "",
+          "diaplayName": "עיצוב ציטוט",
+          "rout": "/",
+          "color": "white"
+        },
+        {
+          "name": "",
+          "diaplayName": "הוספת סיפור",
+          "rout": "/",
+          "color": "white"
+        },
+        {
+          "name": "",
+          "diaplayName": "אודות",
+          "rout": "/about",
+          "tooltip": "עלינו...",
+          "color": "white"
+        },],
+    },
   }
 
-  render() {
+  return (
+    <div className="header-container">
+      <nav className="navbar navbar-expand-lg">
+        {data.header.links.filter(isSignedIn ? (link => !(link.diaplayName == "התחבר")) : (link => !(link.diaplayName == "התנתק")))
+          .map((link, index) => <ul><li className="nav-item">
+            <NavLink style={{ color: "white", display: "flex", flexDirection: "row", justifyContent: "center" }} className="nav-link link-dark" to={link.rout}>
+              {link.diaplayName}
+            </NavLink></li></ul>)}
+      </nav>
+    </div>
+  );
 
-    let data = {
-      "header": {
-        "color": "blue",
-        "logo": "https://brave-together.com/wp-content/uploads/2020/04/%D7%A2%D7%95%D7%AA%D7%A7-%D7%A9%D7%9C-Copy-of-%D7%9C%D7%95%D7%92%D7%95-%D7%97%D7%93%D7%A9-%D7%A8%D7%A7%D7%A2-%D7%A9%D7%A7%D7%95%D7%A3.png",
-        "links": [
-          {
-            "name": "",
-            "diaplayName": "התחבר",
-            "rout": "/login",
-            "color": "white"
-          },
-          {
-            "name": "",
-            "diaplayName": "התנתק",
-            "rout": "/logout",
-            "color": "white"
-          },
-          {
-            "name": "",
-            "diaplayName": "בית",
-            "rout": "/",
-            "color": "white"
-          },
-          {
-            "name": "",
-            "diaplayName": "עיצוב ציטוט",
-            "rout": "/",
-            "color": "white"
-          },
-          {
-            "name": "",
-            "diaplayName": "הוספת סיפור",
-            "rout": "/",
-            "color": "white"
-          },
-          {
-            "name": "",
-            "diaplayName": "אודות",
-            "rout": "/about",
-            "tooltip": "עלינו...",
-            "color": "white"
-          },],
-      },
-    }
+}
 
-    return (
-      <div className="header-container">
-        <nav className="navbar navbar-expand-lg">
-          <ul>
-            <li className="nav-item">
-              <NavLink className="nav-link link-dark" to={data.header.links[0].rout}>
-                {data.header.links[0].diaplayName}
-              </NavLink>
-            </li>
-            {/* // Logout button:
-            <li className="nav-item">
-              <NavLink className="nav-link link-dark" to={headerData.header.links[1].rout}>
-                {headerData.header.links[1].diaplayName}
-              </NavLink>
-            </li> */}
-            <li className="nav-item">
-              <NavLink className="nav-link link-dark" to={data.header.links[2].rout}>
-                {data.header.links[2].diaplayName}
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link link-dark" to={data.header.links[3].rout}>
-                {data.header.links[3].diaplayName}
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link link-dark" to={data.header.links[4].rout}>
-                {data.header.links[4].diaplayName}
-              </NavLink>
-            </li>
-            <li className="nav-item">
-              <NavLink className="nav-link link-dark" to={data.header.links[5].rout}>
-                {data.header.links[5].diaplayName}
-              </NavLink>
-            </li>
-          </ul>
-          <ul>
-            <li className="nav-item">
-              <NavLink to="/">
-                <img src={data.header.logo} alt="לוגו" />
-              </NavLink>
-            </li>
-          </ul>
-        </nav>
-      </div>
-    );
-  }
-};
-
-export default Header;
+export default Header
