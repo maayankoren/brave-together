2d260c07055adcf8f7c6b9e4319d15dee8073275
Yakov Kantor <80357569+yakov10105@users.noreply.github.com>
2021-11-06 21:48:23 +0200

    .

--- client/src/protectedRoute/ProtectedRoute.js
+++ client/src/protectedRoute/ProtectedRoute.js
@@ -2,13 +2,20 @@ import React from 'react'
 import { Redirect, Route } from 'react-router'
 
 const ProtectedRoute = ({component : Component , ...rest} ) => {
+
+    const checkAuthOnServer = ()=>{
+        //calling the server to validate the access key
+    }
+
     return (
         <Route 
             {...rest} 
             render={
             (props)=>{
                 if(localStorage.getItem('authentication-key')){
-                    return <Component {...props}/>
+                    if(checkAuthOnServer()){
+                        return <Component {...props}/>
+                    }
                 }
                 else{
                     return <Redirect to="/unauthorized"/>
