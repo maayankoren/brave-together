d8864b717408bdfdc49412923d19066b918a0ca0
Ariye Caspi <caspi_j@hotmail.com>
2021-12-09 23:19:55 +0200

    .

--- client/src/login/login.js
+++ client/src/login/login.js
@@ -13,27 +13,26 @@ import FormControlLabel from '@mui/material/FormControlLabel';
 import Checkbox from '@mui/material/Checkbox';
 import { NavLink } from 'react-router-dom';
 import Button from '@mui/material/Button';
-import { createTheme } from '@mui/material/styles';
-import { FacebookIcon, } from 'react-share';
+import { MuiThemeProvider, createTheme } from '@material-ui/core/styles';
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            light: '#757ce8',
+            main: '#3f50b5',
+            dark: '#002884',
+            contrastText: '#fff',
+        },
+        secondary: {
+            light: '#ff7961',
+            main: '#f44336',
+            dark: '#ba000d',
+            contrastText: '#000',
+        },
+    },
+});
 
 export default function Login() {
-
-    const theme = createTheme({
-        palette: {
-            primary: {
-                light: '#757ce8',
-                main: '#3f50b5',
-                dark: '#002884',
-                contrastText: '#fff',
-            },
-            secondary: {
-                light: '#ff7961',
-                main: '#f44336',
-                dark: '#ba000d',
-                contrastText: '#000',
-            },
-        },
-    });
 
     const [values, setValues] = useState({
         email: '',
@@ -67,70 +66,73 @@ export default function Login() {
     }
 
     return (
+        <MuiThemeProvider theme={theme}>
 
-        <Box
-            sx={{
-                mx: 'auto',
-                px: 2,
-                pt: 3,
-                textAlign: 'center',
-                fontWeight: 'medium',
-                backgroundColor: 'white',
-            }}
-            dir="rtl"
-        >
-            <Box sx={{ fontSize: 20 }}>{data.header.content.join('\n')}</Box>
-            <img src="/logo1.png" alt="logo" width="220px" height="90px" />
-            <TextField fullWidth id="email" label="אימייל" variant="outlined" margin="normal" />
+            <Box
+                sx={{
+                    mx: 'auto',
+                    px: 2,
+                    pt: 3,
+                    textAlign: 'center',
+                    fontWeight: 'medium',
+                    backgroundColor: 'white',
+                }}
+                dir="rtl"
+            >
+                <Box sx={{ fontSize: 20 }}>{data.header.content.join('\n')}</Box>
+                <img src="/logo1.png" alt="logo" width="220px" height="90px" />
+                <TextField fullWidth id="email" label="אימייל" variant="outlined" margin="normal" />
 
-            <FormControl fullWidth variant="outlined" margin="normal">
-                <InputLabel htmlFor="outlined-adornment-password">סיסמה</InputLabel>
-                <OutlinedInput
-                    id="outlined-adornment-password"
-                    type={values.showPassword ? 'text' : 'password'}
-                    value={values.password}
-                    onChange={handleChange('password')}
-                    endAdornment={
-                        <InputAdornment position="end">
-                            <IconButton
-                                aria-label="toggle password visibility"
-                                onClick={handleClickShowPassword}
-                                onMouseDown={handleMouseDownPassword}
-                                edge="end"
-                            >
-                                {values.showPassword ? <VisibilityOff /> : <Visibility />}
-                            </IconButton>
-                        </InputAdornment>
-                    }
-                    label="סיסמה"
-                />
-            </FormControl>
-            <FormGroup style={{ display: "flex", flexDirection: "row", justifyContent: "space-between", fontSize: "13px" }}>
-                <FormControlLabel control={<Checkbox style={{ marginTop: "-1" }} />} label="זכור אותי" />
-                <NavLink to={"/"} style={{ margin: "9px", color: "#c53d13", textDecoration: "none" }}>שכחת סיסמה?</NavLink>
-            </FormGroup>
-            <Button variant="contained" color={theme.pallate.secondary}
-                style={{ marginTop: "10px", width: "100%", height: "50px", fontSize: "20px" }}>התחבר</Button>
+                <FormControl fullWidth variant="outlined" margin="normal">
+                    <InputLabel htmlFor="outlined-adornment-password">סיסמה</InputLabel>
+                    <OutlinedInput
+                        id="outlined-adornment-password"
+                        type={values.showPassword ? 'text' : 'password'}
+                        value={values.password}
+                        onChange={handleChange('password')}
+                        endAdornment={
+                            <InputAdornment position="end">
+                                <IconButton
+                                    aria-label="toggle password visibility"
+                                    onClick={handleClickShowPassword}
+                                    onMouseDown={handleMouseDownPassword}
+                                    edge="end"
+                                >
+                                    {values.showPassword ? <VisibilityOff /> : <Visibility />}
+                                </IconButton>
+                            </InputAdornment>
+                        }
+                        label="סיסמה"
+                    />
+                </FormControl>
+                <FormGroup style={{ display: "flex", flexDirection: "row", justifyContent: "space-between", fontSize: "13px" }}>
+                    <FormControlLabel control={<Checkbox style={{ marginTop: "-1" }} />} label="זכור אותי" />
+                    <NavLink to={"/"} style={{ margin: "9px", color: "#c53d13", textDecoration: "none" }}>שכחת סיסמה?</NavLink>
+                </FormGroup>
+                <Button variant="contained" color="secondary"
+                    style={{ marginTop: "10px", width: "100%", height: "50px", fontSize: "20px" }}>התחבר</Button>
 
-            <FormGroup style={{ display: "flex", flexDirection: "row", justifyContent: "space-evenly", fontSize: "13px" }}>
-                <hr size="5" color="#000000" width="90px" noshade style={{ marginTop: "29px" }} />
-                <br />או באמצעות
-                <hr size="5" color="#000000" width="90px" noshade style={{ marginTop: "29px" }} />
-            </FormGroup>
+                <FormGroup style={{ display: "flex", flexDirection: "row", justifyContent: "space-evenly", fontSize: "13px" }}>
+                    <hr size="5" color="#000000" width="90px" noshade style={{ marginTop: "29px" }} />
+                    <br />או באמצעות
+                    <hr size="5" color="#000000" width="90px" noshade style={{ marginTop: "29px" }} />
+                </FormGroup>
 
-            <FormGroup style={{ display: "flex", flexDirection: "row", justifyContent: "space-evenly", fontSize: "13px" }}>
-                <img src="/google icon.png" alt="google" style={{ width: "75px" }} />
-                <img src="/facebook icon.png" alt="facebook" style={{ width: "75px" }} />
-            </FormGroup>
+                <FormGroup style={{ display: "flex", flexDirection: "row", justifyContent: "space-evenly", fontSize: "13px" }}>
+                    <img src="/google icon.png" alt="google" style={{ width: "75px" }} />
+                    <img src="/facebook icon.png" alt="facebook" style={{ width: "75px" }} />
+                </FormGroup>
 
-            <Button variant="contained" color="secondary"
-                style={{ marginTop: "10px", width: "100%", height: "50px", fontSize: "20px" }}>הירשם</Button>
-<div style={{margin: "10px"}}>
-<NavLink to={"/"} style={{ color: "black", textDecoration: "none", margin: "10px" }}>דלג לבינתיים</NavLink>
-</div>
+                <Button variant="contained" color="error"
+                    style={{ marginTop: "10px", width: "100%", height: "50px", fontSize: "20px" }}>הירשם</Button>
+                <div style={{ margin: "10px" }}>
+                    <NavLink to={"/"} style={{ size: "50px", color: "black", textDecoration: "none" }}>דלג לבינתיים</NavLink>
+                </div>
 
-        </Box>
+            </Box>
 
 
+        </MuiThemeProvider>
     );
+
 }
