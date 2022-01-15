839785cbb6cc30bbab353e637a4d5abd74b33266
Anat12527 <eilamanat12@gmail.com>
2022-01-15 21:21:55 +0200

    new header

--- client/src/header/header.js
+++ client/src/header/header.js
@@ -1,97 +1,32 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import "./header.scss";
+import menu1 from "../assets/images/menu1.png";
+import Logo from "../assets/images/Logo.jpg";
 
-function Header() {
+export const Header = () => {
+  const [isNavOpen, setIsNavOpen] = useState(false)
 
-  const [isSignedIn, setSignIn] = useState(false);
-
-  useEffect(() => {
-    let token = localStorage.getItem('token');
-
-    if (token) {
-      setSignIn(true);
-    } else {
-      setSignIn(false);
-    }
-  }, [])
-
-  // var data = () => fetch('http://127.0.0.1:5000/header')
-  // .then(response => response.json())
-  // // .then((jsonData) => {
-  // //   console.log(jsonData)
-  // // })
-  // .catch((error) => {
-  //   console.error(error)
-  // });
-
-  // async function getDataFromServer() {
-  //   try {
-  //     let response = await fetch('http://127.0.0.1:5000/header');
-  //     let responseJson = await response.json();
-  //     return responseJson;
-  //    } catch(error) {
-  //     console.error(error);
-  //   }
-  // }
-
-  let data = {
-    "header": {
-      "color": "blue",
-      "logo": "https://brave-together.com/wp-content/uploads/2020/04/%D7%A2%D7%95%D7%AA%D7%A7-%D7%A9%D7%9C-Copy-of-%D7%9C%D7%95%D7%92%D7%95-%D7%97%D7%93%D7%A9-%D7%A8%D7%A7%D7%A2-%D7%A9%D7%A7%D7%95%D7%A3.png",
-      "links": [
-        {
-          "name": "",
-          "diaplayName": "התחבר",
-          "rout": "/login",
-          "color": "white"
-        },
-        {
-          "name": "",
-          "diaplayName": "התנתק",
-          "rout": "/",
-          "color": "white"
-        },
-        {
-          "name": "",
-          "diaplayName": "בית",
-          "rout": "/",
-          "color": "white"
-        },
-        {
-          "name": "",
-          "diaplayName": "עיצוב ציטוט",
-          "rout": "/",
-          "color": "white"
-        },
-        {
-          "name": "",
-          "diaplayName": "הוספת סיפור",
-          "rout": "/",
-          "color": "white"
-        },
-        {
-          "name": "",
-          "diaplayName": "אודות",
-          "rout": "/about",
-          "tooltip": "עלינו...",
-          "color": "white"
-        },],
-    }
+  const onToggleModal = () => {
+    setIsNavOpen(!isNavOpen)
   }
 
   return (
-    <div className="header-container">
-      <nav className="navbar navbar-expand-lg">
-        {data.header.links.filter(isSignedIn ? (link => !(link.diaplayName == "התחבר")) : (link => !(link.diaplayName == "התנתק")))
-          .map((link, index) => <ul><li className="nav-item">
-            <NavLink style={{ color: "white", display: "flex", flexDirection: "row", justifyContent: "center" }} className="nav-link link-dark" to={link.rout}>
-              {link.diaplayName}
-            </NavLink></li></ul>)}
-      </nav>
-    </div>
-  );
+    <header className="app-header">
+      <div className="user-container">User</div>
+      <div className="logo"><img src={Logo}/></div>
+      <div className="nav-btn" onClick={onToggleModal}><img className="hamburger" src={menu1}/></div>
 
+      <nav className={"navbar-container " + (isNavOpen ? 'open' : '')}>
+            <div className="nav-item"> <NavLink style={{ textDecoration: 'none',color:'black' }} to="">אודות</NavLink></div>
+           
+            <div className="nav-item"><NavLink style={{ textDecoration: 'none',color:'black' }} to="">שמורים</NavLink></div>
+            <div className="nav-item"><NavLink  style={{ textDecoration: 'none',color:'black' }} to="">פרויקטים נוספים</NavLink></div>
+            <div className="nav-item"><NavLink  style={{ textDecoration: 'none',color:'black' }} to="">פעילויות העמותה</NavLink></div>
+            <div className="nav-item"><NavLink style={{ textDecoration: 'none',color:'black' }} to="">לתרומות</NavLink></div>
+            <div className="nav-item"><NavLink style={{ textDecoration: 'none',color:'black' }} to="">הגדרות</NavLink></div>
+      </nav>
+    </header>
+  )
 }
 
-export default Header;
